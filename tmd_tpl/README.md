cf12b87cfd910a7355438f9a868c0af00f759ead
lihui_luo <lihui_luo@kingdee.com>
2016-05-02 23:54:20 +0800

    modify tmd_tpl

--- tmd_tpl/README.md
+++ tmd_tpl/README.md
@@ -5,5 +5,54 @@ php模板引擎
 	来源：http://www.tmdphp.com/
 	
 ## tmd_tpl1.php
-	基于tmd_tpl修改，主要是将引用的include/require的模板文件合并在主文件里。
-	比如，mail.tpl里包含header.tpl,content.tpl,footer.tpl，生成缓存的时候，在mail.tpl一个文件里会包含header.tpl,content.tpl,footer.tpl的内容。
+	基于tmd_tpl修改，增加功能特性如下：
+	1.增加assignObj($obj)方法。这样把整个controller/action对象塞进去，就不需要每个变量都assign，如
+	$name = 'frogluo';
+	$age = 32;
+	$uid = 123456;
+	$is_admin = 0;
+	$this->view->assign('name',$name);
+	$this->view->assign('age',$age);
+	$this->view->assign('uid',$uid);
+	$this->view->assign('is_admin',$is_admin);
+	
+	改成：
+	$this->name = 'frogluo';
+	$this->age = 32;
+	$this->uid = 123456;
+	$this->is_admin = 0;
+	$this->display();
+	
+	/**
+	* 父类先封装一个方法，当调用display时才初始化视图
+	*/
+	protected function display($tpl = '') {
+		$this->view = new tmd_tpl ();
+		$this->view->tpl_dir = PATH_PAGE_VIEW;
+		$this->view->cache_dir = PATH_PAGE_CACHE;
+		$this->view->cache_time = TPL_CACHE_TIME;
+		$this->view->my_rep = array (
+				'~__ROOT__~' => URL_HOST,
+				'~__JSPATH__~' => URL_JS,
+				'~__CSSPATH__~' => URL_CSS,
+				'~__PLUGINPATH__~' => URL_PLUGIN,
+				'~__IMAGEPATH__~' => URL_IMAGE,
+				'~__VERSION__~' => UPDATE_TIME 
+		);
+		$this->view->assignobj ( $this );
+		
+		if ($tpl) {
+			$this->view->display ( $tpl );
+		} else {
+			if (! $this->page_tpl) {
+				$this->page_tpl = 'index.html';
+			}
+			$this->view->display ( $this->page_frame );
+		}
+		exit ();
+	}
+	
+	2.将引用的include/require的模板文件合并在主文件里。
+	比如，mail.tpl里包含header.tpl,content.tpl,footer.tpl，
+	生成缓存的时候，在mail.tpl一个文件里会包含header.tpl,content.tpl,footer.tpl的内容。
+	这样做，提升一些性能，特别是页面分得比较零碎的时候。
--- tmd_tpl/tmd_tpl.php
+++ tmd_tpl/tmd_tpl.php
@@ -1,10 +1,9 @@
 <?php
 
 class tmd_tpl {
-
     public $tpl_dir = './tpl/';
     // 模板文件所在目录 注意以斜杠结束
-    public $tpl_ext = '';
+    public $tpl_ext = '.php';
     // 模板文件扩展名 建议为PHP文件 这样在Dreamweaver中PHP代码才会有高亮效果
     public $cache_dir = './tpl_c/';
     // 模板编译后的缓存目录 一样以斜杠结束 无则自动创建
@@ -12,33 +11,44 @@ class tmd_tpl {
     // 编译后模板的缓存时间（单位：秒）
     // 0为马上过期 即每次都重新编译
     // -1为永不过期 网站上线后建议设置为-1
-    
+
     // 自定义的正则替换
-    public $my_rep;
-    
+    public $my_rep = array(
+        '~(\.\./)+static~' => '/static',
+        '~__UP__~' => '/static/upload',
+//        '~__PROJ__~' => '/index.php',
+//        '~__SPACE__~' => '/index.php/home',
+//        '~__CLASS__~' => '/index.php/home/index',
+//        '~__METHOD__~' => '/index.php/home/index/index',
+    );
     // 内置的正则替换
     private $tmd_rep = array(
-        '~\{(\$[a-z0-9_]+)\}~i' => '<?php echo isset($1)?$1:\'\'; ?>', // {$name}
-        '~\{(\$[a-z0-9_]+)\.([a-z0-9_]+)\}~i' => '<?php echo isset($1[\'$2\'])?$1[\'$2\']:\'\'; ?>', // {$arr.key}
-        '~\{(\$[a-z0-9_]+)\[[\'\"]([a-z0-9_]+)[\'\"]\]\}~i' => '<?php echo isset($1[\'$2\'])?$1[\'$2\']:\'\'; ?>', // {$arr.key}
-        '~\{(\$[a-z0-9_]+)\.([a-z0-9_]+)\.([a-z0-9_]+)\}~i' => '<?php echo isset($1[\'$2\'][\'$3\'])?$1[\'$2\'][\'$3\']:\'\'; ?>', // {$arr.key.key2}
-        '~\{(\$[a-z0-9_]+)\[[\'\"]([a-z0-9_]+)[\'\"]\]\[[\'\"]([a-z0-9_]+)[\'\"]\]\}~i' => '<?php echo isset($1[\'$2\'][\'$3\'])?$1[\'$2\'][\'$3\']:\'\'; ?>', // {$arr.key.key2}
-        '~<\?php\s+(include_once|require_once|include|require)\s*\(\s*(.+?)\s*\)\s*;?\s*\?>~i' => '<?php include \$this->_include($2) ?>', // ＜?php include('inc/top.php'); ?＞
+        '~\{(\$[a-z0-9_]+)\}~i'
+            => '<?php echo $1 ?>', // {$name}
+        
+        '~\{(\$[a-z0-9_]+)\.([a-z0-9_]+)\}~i'
+            => '<?php echo $1[\'$2\'] ?>', // {$arr.key}
+        
+        '~\{(\$[a-z0-9_]+)\.([a-z0-9_]+)\.([a-z0-9_]+)\}~i'
+            => '<?php echo $1[\'$2\'][\'$3\'] ?>', // {$arr.key.key2}
+        
+        '~<\?php\s+(include_once|require_once|include|require)\s*\(\s*(.+?)\s*\)\s*;?\s*\?>~i'
+            => '<?php include \$this->_include($2, __FILE__) ?>', // ＜?php include('inc/top.php'); ?＞
+        
         '~\{:(.+?)\}~' => '<?php echo $1 ?>', // {:strip_tags($a)}
+        
         '~\{\~(.+?)\}~' => '<?php $1 ?>', // {~var_dump($a)}
-        '~<\?=\s*~' => '<?php echo '
+        
+        '~<\?=\s*~' => '<?php echo ', // <?=
     );
- // <?=
-                                    
     // 用于存储模板变量
     public $data = array();
-
-    function __construct($cfg = NULL) {
+    
+    function __construct($cfg=NULL) {
         if ($cfg) {
             $this->config($cfg);
         }
     }
-
     function config($cfg) {
         if (is_string($cfg)) {
             $cfg = require $cfg;
@@ -63,27 +73,19 @@ class tmd_tpl {
         }
     }
     // 赋值
-    function assign($name, $value = NULL) {
+    function assign($name, $value=NULL) {
         if (is_array($name)) {
             foreach ($name as $k => $v) {
                 $this->data[$k] = $v;
             }
-        } else {
+        }else{
             $this->data[$name] = &$value;
-        }
-    }
-    
-    // 将类的公共变量赋值
-    function assignObj($obj) {
-        $data = get_object_vars($obj);
-        foreach ($data as $key => $value) {
-            $this->Assign($key, $value);
         }
     }
     // 输出页面
     function display($tpl_file) {
         $_cache_path = $this->cache_path($tpl_file);
-        if (! $this->is_cached($_cache_path)) {
+        if (!$this->is_cached($_cache_path)) {
             $this->compile($this->tpl_path($tpl_file), $_cache_path);
         }
         unset($tpl_file);
@@ -107,44 +109,42 @@ class tmd_tpl {
     }
     // 模板缓存是否有效
     private function is_cached($cache_path) {
-        if (! file_exists($cache_path)) {
+        if (!file_exists($cache_path)) {
             return false;
         }
-        if ($this->cache_time < 0) {
+        if ($this->cache_time<0) {
             return true;
         }
         $cache_time = filemtime($cache_path);
-        if (time() - $cache_time > $this->cache_time) {
+        if ( time()-$cache_time > $this->cache_time ) {
             return false;
         }
         return true;
     }
     // 编译模板
     private function compile($tpl_path, $cache_path) {
-        $tpl = file_get_contents($tpl_path);
-        if ($tpl === FALSE) {
+        $tpl = @file_get_contents($tpl_path);
+        if ($tpl===FALSE) {
             die("模板文件“{$tpl_path}”不存在");
         }
         
         $tmp = array_merge($this->tmd_rep, $this->my_rep);
         $cache = preg_replace(array_keys($tmp), $tmp, $tpl);
-        $path = dirname($cache_path);
-        if (! file_exists($path)) {
-            mkdir($path, 0777, true);
-        }
         
-        $tmp = file_put_contents($cache_path, $cache, LOCK_EX);
-        if ($tmp === FALSE) {
+        @mkdir(dirname($cache_path), 0777, true);
+        
+        $tmp = @file_put_contents($cache_path, $cache, LOCK_EX);
+        if ($tmp===FALSE) {
             die("编译后的模板文件“$cache_path”无法写入");
         }
     }
     // 页面有include时用到
-    private function _include($inc_file) {
-        $cache_path = $this->cache_dir . $inc_file;
-        $tpl_path = $this->tpl_dir . $inc_file;
-        if (! $this->is_cached($cache_path)) {
-            $this->compile($tpl_path, $cache_path);
+    private function _include($inc_file, $cache_path) {
+        $inc_path = dirname($cache_path) . '/' . $inc_file;
+        if (!$this->is_cached($inc_path)) {
+            $tpl_path = str_replace(realpath($this->cache_dir), realpath($this->tpl_dir), $inc_path);
+            $this->compile($tpl_path, $inc_path);
         }
-        return $cache_path;
+        return $inc_path;
     }
 }
